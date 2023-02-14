d316d115e74df622380a0f9c247a19bee21c1757
cpenas94 <henrypenas94@gmail.com>
2023-02-13 20:17:56 -0800

    cleaned up

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-$(document).ready(function()    {
+$(document).ready(function() {
     $("#animate").on("click", animate);
-});
-
-function animate () {
+  });
+  
+  function animate() {
     $("#header").animate({
-        fontSize: 64,
-        color: jQuery.Color({ saturation: 1, lightness: 0.5 })
+      fontSize: 64,
+      color: jQuery.Color({
+        saturation: 1,
+        lightness: 0.5
+      })
     }, 1000).animate({
-        color: jQuery.Color({ hue: 250 })
+      color: jQuery.Color({
+        hue: 250
+      })
     }, 1500, function() {
-
-        setTimeout(function() {
-            $('#header').animate({
-                fontSize: 32,
-                color: jQuery.Color({ lightness: 0 })
-            }, 1000)
-        }, 1500);
+  
+      setTimeout(function() {
+        $('#header').animate({
+          fontSize: 32,
+          color: jQuery.Color({
+            lightness: 0
+          })
+        }, 1000)
+      }, 1500);
     });
-
+  
     $("#image").animate({
-        "border-width": 10,
-        "border-color": jQuery.Color({ saturation: 1, lightness: 0.5 })
+      "border-width": 10,
+      "border-color": jQuery.Color({
+        saturation: 1,
+        lightness: 0.5
+      })
     }, 1000).animate({
-        "border-color": jQuery.Color({ hue: 250 })
+      "border-color": jQuery.Color({
+        hue: 250
+      })
     }, 1500, function() {
-
-        setTimeout(function() {
-            $("#image").animate({
-                "border-width": 0,
-                "border-color": jQuery.Color({ lightness: 0 })
-            }, 1000)
-        }, 1500);
+  
+      setTimeout(function() {
+        $("#image").animate({
+          "border-width": 0,
+          "border-color": jQuery.Color({
+            lightness: 0
+          })
+        }, 1000)
+      }, 1500);
     });
-}
+  }
+  
