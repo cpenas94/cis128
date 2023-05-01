2e72000894b0d55f179ba25a9c5a166bcefa2aba
cpenas94 <henrypenas94@gmail.com>
2023-04-30 22:38:12 -0700

    test

--- homework4/service-worker.js
+++ homework4/service-worker.js
@@ -1,46 +1,21 @@
-var cacheName = 'pwa-cache';
-var appShellFiles = [
-  'index.html',
-  'main.css',
-  'background.js',
-  '/img/lightblue.jpg',
-  '/img/lightgold.jpg',
-];
+const cacheName = 'pwa-cache';
 
-self.addEventListener('install', function(e) {
-  console.log('[Service Worker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-        console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll(appShellFiles);
-    })
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+        '/index.html',
+        '/style.css',
+        '/background.js',
+        '/img/lightblue.jpg',
+        '/img/lightgold.jpg'
+      ]))
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then(function(response) {
-                return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
-
-// Clear anything we don't need from the cache
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-          return Promise.all(keyList.map(function(key) {
-        if(cacheName.indexOf(key) === -1) {
-          return caches.delete(key);
-        }
-      }));
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
